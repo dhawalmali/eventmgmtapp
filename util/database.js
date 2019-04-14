9a16925c07d837bd45c725336ee4b88387834bdd
dhawalmali <dhawal.mali@yahoo.com>
2019-04-14 22:44:51 +0530

    db auth

--- util/database.js
+++ util/database.js
@@ -1,7 +1,15 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('event_mgmt_app','root','',{
-    dialect: 'mysql',
-    host: 'localhost'
-});
 
+const sequelize = new Sequelize('event_mgmt_app', 'root', 'dhawalroot', {
+    host: 'dhawal.czszqruibg4u.ap-south-1.rds.amazonaws.com',
+    port: 3306,
+    logging: console.log,
+    maxConcurrentQueries: 100,
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl:'Amazon RDS'
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30},
+    language: 'en'
+})
 module.exports = sequelize;
